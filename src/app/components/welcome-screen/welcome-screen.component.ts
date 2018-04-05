dec942f5387468dc2790d88360159c3a79a0af03
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2018-04-05 16:20:08 +0100

    adding selecting days

--- src/app/components/welcome-screen/welcome-screen.component.ts
+++ src/app/components/welcome-screen/welcome-screen.component.ts
@@ -7,16 +7,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeScreenComponent implements OnInit {
 
-  // look up why this doesn't need declared as const
-  months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-    'August', 'September', 'October', 'November', 'December'];
+  monthInfo = {
+    'January': 31,
+    'February': 29,
+    'March': 31,
+    'April': 30,
+    'May': 31,
+    'June': 30,
+    'July': 31,
+    'August': 31,
+    'September': 30,
+    'October': 31,
+    'November': 30,
+    'December': 31
+  };
 
-  formHidden = true;
+  months = Object.keys(this.monthInfo);
+
+  welcomeShown = true;
+  monthsShown = false;
+  daysShown = false;
+  thankyouShown = false;
+  monthBorn = '';
+  dayBorn = 0;
+
+  showMonths() {
+    this.welcomeShown = false;
+    this.monthsShown = true;
+    this.daysShown = false;
+  }
+
+  selectMonth(receiver) {
+    this.daysInMonth(this.monthInfo[receiver.target.innerText]);
+    this.welcomeShown = false;
+    this.monthsShown = false;
+    this.daysShown = true;
+    this.monthBorn = receiver.target.innerText;
+  }
+
+  daysInMonth(days) {
+    const listOfDaysInMonth = Array.from(Array(days).keys()).map( function(value) {
+      return value + 1;
+    } );
+    return listOfDaysInMonth;
+  }
+
+  selectDay(receiver) {
+    this.welcomeShown = false;
+    this.monthsShown = false;
+    this.daysShown = false;
+    this.thankyouShown = true;
+    this.dayBorn = receiver.target.innerText;
+  }
 
   ngOnInit() {
   }
 
-  showForm() {
-    this.formHidden = !this.formHidden;
-  }
 }
