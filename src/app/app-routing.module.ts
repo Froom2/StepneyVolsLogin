ceca695e0c52d3e1fd5b82c2a9f52f54e65f9a44
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2018-06-19 19:22:20 +0100

    auth: adding basic page and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule} from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
 import { WelcomeScreenComponent } from './components/welcome-screen/welcome-screen.component';
 
 const appRoutes: Routes = [
-  { path: '', component: WelcomeScreenComponent}
+  { path: '', component: AuthenticationComponent},
+  { path: 'login', component: WelcomeScreenComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormElements, WelcomeScreenComponent} from './components/welcome-screen/welcome-screen.component';
 import {AppRoutingModule} from './app-routing.module';
+import { AuthenticationComponent } from './components/authentication/authentication.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeScreenComponent
+    WelcomeScreenComponent,
+    AuthenticationComponent
   ],
   imports: [
     BrowserModule,
